dc88ca30da9745d7092ea27506fe1053791a84c7
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-08 18:18:34 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        float num1 = (float) 9;
+        float num2 = (float) 5;
+        float num5 = num1/num2;
+        int num3 = 41;
+        int num4 = 32;
+        System.out.println(num5*num3+num4);
 
+    }
+}
