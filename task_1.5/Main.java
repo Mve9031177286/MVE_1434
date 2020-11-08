14fb35f963951515b0726e419288993e45cbab3d
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-08 19:35:42 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
         float num1 = (float) 9;
         float num2 = (float) 5;
         float num5 = num1/num2;
-        int num3 = 41;
+        int num3 = scan.nextInt();
         int num4 = 32;
         System.out.println(num5*num3+num4);
 
