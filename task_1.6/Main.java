c2b6bb2e0bad281596aa4d382722c21417157a67
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-08 18:33:44 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -6,3 +6,16 @@ public class Main {
         System.out.println(num1+num3);
     }
 }
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 15;
+        System.out.println(num1/num1*num2+num1);
+
+    }
+}
