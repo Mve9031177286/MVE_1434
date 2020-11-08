93c7b9ad34434c85f6fbd1947f8ee7f74dd91ba0
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-08 18:01:55 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -6,3 +6,15 @@ public class Main {
 
     }
 }
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 25;
+        System.out.println(number*number);
+
+    }
+}
