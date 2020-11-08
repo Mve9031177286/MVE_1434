bcfdd25a19c74b121592292ffbc5d4801dfa8f4a
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-08 19:43:04 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,21 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int num1 = 100;
+        Scanner scan = new Scanner (System.in);
+        int num1 = scan.nextInt();
         int num2 = 15;
-        int num3 = num1/num1*num2;
-        System.out.println(num1+num3);
-    }
-}
-
-
-
-
-
-public class Main {
-    public static void main(String[] args) {
-        int num1 = 100;
-        int num2 = 15;
-        System.out.println(num1/num1*num2+num1);
+        int num3 = num1/num1;
+        int num4 = num3*num2;
+        System.out.println(num1+num4);
 
     }
 }
