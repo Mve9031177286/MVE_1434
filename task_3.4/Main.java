60d9ec2064c19a7c6f76341ace99d505e32dc741
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-10 18:27:53 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,15 @@
+
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+
+public class Main {
+    public static boolean main(String[] args) {
+        public static boolean func ( int a, int b) {
+            if (a + b)>10 return true;
+        else return false;
+        }
+
+
+    }
+}
