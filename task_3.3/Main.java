ebfcba2b526d9599be169d1b0ed4bdac26b3c44a
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-10 16:27:31 +0300

    Create Main.java

---
+++ task_3.3/Main.java
@@ -0,0 +1,19 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        String c = "True";
+        String d = "False";
+    }
+        public static int getMin ( int a, int b) {
+            if (a == b) return c;
+            System.out.println(c);
+            else return d;
+            System.out.println(d);
+        }
+}
