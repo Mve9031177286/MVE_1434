7202c55d4185e4ad8f129ed7bf98a33e8a93ce83
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-13 13:24:45 +0300

    Update Main.java

--- task_4.1/Main.java
+++ task_4.1/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.setName("Жужик");
-        System.out.println(cat.name);
+        System.out.println(cat.name());
     }
 }
 
