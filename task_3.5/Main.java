0e958872d692ff881ca0b623ccf87b6542722d7e
Mve9031177286 <74023712+Mve9031177286@users.noreply.github.com>
2020-11-16 18:58:46 +0300

    Create Main.java

---
+++ task_3.5/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Дан массив с числами. Выведите последовательно его элементы используя рекурсию и не используя цикл.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        int num[] = {4,2,6,2,65};
+                System.out.println(num[0]);
+                System.out.println(num[0+1]);
+                System.out.println(num[0+2]);
+                System.out.println(num[0+3]);
+                System.out.println(num[0+4]);
+    }
+}
